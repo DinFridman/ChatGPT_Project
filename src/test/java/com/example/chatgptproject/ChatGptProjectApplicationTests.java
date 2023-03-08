f9dc529b00db5c69117b6a70589c4781c1c41412
DinFridman <din121222@gmail.com>
2023-03-08 14:35:06 +0200

    initially commit

---
+++ src/test/java/com/example/chatgptproject/ChatGptProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.chatgptproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatGptProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
