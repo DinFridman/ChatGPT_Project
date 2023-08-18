fa0f4e11374b0548e535087abb0d4d579f1085ac
DinFridman <din121222@gmail.com>
2023-08-18 15:53:46 +0300

    dependencies updated.
    dependencies updated.

--- src/main/java/com/example/chatgptproject/model/AppUserEntity.java
+++ src/main/java/com/example/chatgptproject/model/AppUserEntity.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateTimeDeserializer;
 import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
+import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import lombok.Data;
